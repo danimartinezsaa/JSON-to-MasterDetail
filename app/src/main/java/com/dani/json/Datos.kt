181d354d725a32ddbb9a38fbc60a76ee007bf7ad
Dani Martínez <32169739+danimartinezsaa@users.noreply.github.com>
2019-01-30 11:34:34 +0100

    Delete Datos.kt
    Clase sustituida por Object Class

--- app/src/main/java/com/dani/json/Datos.kt
+++
@@ -1,9 +0,0 @@
-package com.dani.json
-
-data class Datos(val titulo : String, val descripcion : String){
-    // personalizamos to String
-    override fun toString(): String {
-        return "El título es: $titulo"
-    }
-
-}
